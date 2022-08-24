7b176c0780fa3b2d0f8c6779791c0e205ac33820
TimohaKodit <63901163+TimohaKodit@users.noreply.github.com>
2022-08-24 13:13:15 +0500

    Add files via upload

---
+++ checker.py
@@ -0,0 +1,17 @@
+
+
+with open("F:\\tt.txt", "r") as file:
+    lines = [i.strip() for i in file]
+
+with open("F:\\bb.txt", "r") as file:
+    line = [i.strip() for i in file]
+
+count = []
+
+for i in lines:
+    if i in line:
+        count.append(i)
+    else:
+        continue 
+if len(count) > 0:
+    print("Совпадения найдены: ",', '.join(count))
